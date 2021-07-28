6eea02432e60584049a9c8b0ffc3778b7ff20eb9
atamakonkurii <morisudesu@gmail.com>
2021-07-29 08:32:11 +0900

    リアルタイムプレビューを適用

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,9 +1,16 @@
+require 'rouge/plugins/redcarpet'
+
+class HTML < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+end
+
 module MarkdownHelper
   def markdown(text)
     options = {
-      filter_html:     true,
-      hard_wrap:       true,
-      space_after_headers: true
+      filter_html: true,
+      hard_wrap: true,
+      space_after_headers: true,
+      with_toc_data: true
     }
 
     extensions = {
@@ -12,11 +19,23 @@ module MarkdownHelper
       lax_spacing: true,
       no_intra_emphasis: true,
       strikethrough: true,
+      underline: true,
+      highlight: true,
       superscript: true,
       tables: true
     }
     renderer = Redcarpet::Render::HTML.new(options)
     @markdown = Redcarpet::Markdown.new(renderer, extensions)
     @markdown.render(text).html_safe
+  end
+
+  def toc(text)
+    toc_option = {
+      nesting_level: 2
+    }
+
+    toc_renderer = Redcarpet::Render::HTML_TOC.new
+    toc = Redcarpet::Markdown.new(toc_renderer, toc_option)
+    toc.render(text).html_safe
   end
 end
