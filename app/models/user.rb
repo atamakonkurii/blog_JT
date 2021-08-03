a928f14efad49c0260a9a546b1ea49499ec0d70a
atamakonkurii <morisudesu@gmail.com>
2021-08-04 06:53:48 +0900

    バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :password_digest, presence: true, length: { minimum: 3, maximum: 72 }
 end
