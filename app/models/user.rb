6e44f5bbc32a77e32868ca21c6eaa9fd69af53aa
atamakonkurii <morisudesu@gmail.com>
2021-08-04 07:01:26 +0900

    バリデーションを追加2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
-  validates :password_digest, presence: true, length: { minimum: 3, maximum: 72 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 , maximum: 72}
+  has_secure_password
 end
