c5c1b7107df687e437e1f729328ba5877b0fbc7d
atamakonkurii <morisudesu@gmail.com>
2021-08-04 07:22:56 +0900

    空のテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'name、email、password_diegestが正しくある場合、有効な状態であること' do
+
+  end
+
+  it 'nameが空の時無効な状態であること' do
+
+  end
+
+  it 'nameが30文字より多い時無効な状態であること' do
+
+  end
+
+  it 'emailが空の時無効な状態であること' do
+
+  end
+
+  it 'emailが重複していた場合無効な状態であること' do
+
+  end
+
+  it 'passwordが6文字未満の時無効な状態であること' do
+
+  end
+
+  it 'passwordが72文字より多い時無効な状態であること' do
+
+  end
 end
