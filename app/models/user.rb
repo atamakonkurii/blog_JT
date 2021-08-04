8ac96c0249f111d862bbeed9eab2ec2eecf18048
atamakonkurii <morisudesu@gmail.com>
2021-08-04 22:50:06 +0900

    userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 6, maximum: 72 }
   has_secure_password
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: VALID_PASSWORD_REGEX }
 end
