4dc01491a5b6af8cec77d55094e978f6c902bf2d
atamakonkurii <morisudesu@gmail.com>
2021-08-06 08:01:04 +0900

    enumを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,6 @@ class Article < ApplicationRecord
   has_one :place, dependent: :destroy
   has_many :tags, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  enum main_language: { japanese: 0, taiwanese: 1, english: 2 }
 end
