8d995db47565ebfce00efcef74ba8b108fae3393
atamakonkurii <morisudesu@gmail.com>
2021-08-08 21:01:22 +0900

    rubocopの指摘を修正

--- app/models/prefecture_taiwan.rb
+++ app/models/prefecture_taiwan.rb
@@ -1,12 +1,14 @@
 class PrefectureTaiwan < ActiveHash::Base
-
   self.data = [
-    {id: 1, name: '臺北市', name_en: 'Taipei'}, {id: 2, name: '新北市', name_en: 'New Taipei'}, {id: 3, name: '桃園市', name_en: 'Taoyuan'},
-    {id: 4, name: '臺中市', name_en: 'Taichung'}, {id: 5, name: '臺南市', name_en: 'Tainan'}, {id: 6, name: '高雄市', name_en: 'Kaohsiung'},
-    {id: 7, name: '基隆市', name_en: 'Keelung'}, {id: 8, name: '新竹市', name_en: 'Hsinchu City'}, {id: 9, name: '嘉義市', name_en: 'Chiayi City'},
-    {id: 10, name: '新竹縣', name_en: 'Hsinchu County'}, {id: 11, name: '苗栗縣', name_en: 'Miaoli'}, {id: 12, name: '彰化縣', name_en: 'Changhua'},
-    {id: 13, name: '南投縣', name_en: 'Nantou'}, {id: 14, name: '雲林縣', name_en: 'Yunlin'}, {id: 15, name: '嘉義縣', name_en: 'Chiayi County'},
-    {id: 16, name: '屏東縣', name_en: 'Pingtung'}, {id: 17, name: '宜蘭縣', name_en: 'Yilan'}, {id: 18, name: '花蓮縣', name_en: 'Hualien'},
-    {id: 19, name: '臺東縣', name_en: 'Taitung'}, {id: 20, name: '澎湖縣', name_en: 'Penghu'}
+    { id: 1, name: '臺北市', name_en: 'Taipei' }, { id: 2, name: '新北市', name_en: 'New Taipei' },
+    { id: 3, name: '桃園市', name_en: 'Taoyuan' }, { id: 4, name: '臺中市', name_en: 'Taichung' },
+    { id: 5, name: '臺南市', name_en: 'Tainan' }, { id: 6, name: '高雄市', name_en: 'Kaohsiung' },
+    { id: 7, name: '基隆市', name_en: 'Keelung' }, { id: 8, name: '新竹市', name_en: 'Hsinchu City' },
+    { id: 9, name: '嘉義市', name_en: 'Chiayi City' }, { id: 10, name: '新竹縣', name_en: 'Hsinchu County' },
+    { id: 11, name: '苗栗縣', name_en: 'Miaoli' }, { id: 12, name: '彰化縣', name_en: 'Changhua' },
+    { id: 13, name: '南投縣', name_en: 'Nantou' }, { id: 14, name: '雲林縣', name_en: 'Yunlin' },
+    { id: 15, name: '嘉義縣', name_en: 'Chiayi County' }, { id: 16, name: '屏東縣', name_en: 'Pingtung' },
+    { id: 17, name: '宜蘭縣', name_en: 'Yilan' }, { id: 18, name: '花蓮縣', name_en: 'Hualien' },
+    { id: 19, name: '臺東縣', name_en: 'Taitung' }, { id: 20, name: '澎湖縣', name_en: 'Penghu' }
   ]
 end
