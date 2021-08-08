ff4394e7a6da97e74dd0af4df8736bb531010158
atamakonkurii <morisudesu@gmail.com>
2021-08-08 19:30:17 +0900

    Articleテーブルにplace_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_08_073901) do
+ActiveRecord::Schema.define(version: 2021_08_08_102444) do
 
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title_ja"
     t.text "content_ja"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "title_zh_tw"
     t.text "content_zh_tw"
     t.string "title_en"
     t.text "content_en"
     t.integer "main_language", null: false
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "place_id", null: false
   end
 
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -36,10 +37,9 @@ ActiveRecord::Schema.define(version: 2021_08_08_073901) do
   create_table "places", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "country", null: false
     t.integer "prefecture_japan_id"
-    t.integer "prefecture_taiwan_id"
-    t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "prefecture_taiwan_id"
   end
 
   create_table "tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
