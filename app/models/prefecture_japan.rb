815dd5102a6a66bc6edb88cd4b808203620de907
atamakonkurii <morisudesu@gmail.com>
2021-08-08 17:59:02 +0900

    ActiveHashで日本と台湾の県を追加

---
+++ app/models/prefecture_japan.rb
@@ -0,0 +1,21 @@
+class PrefectureJapan < ActiveHash::Base
+
+  self.data = [
+    {id: 1, name: '北海道', name_en: 'Hokkaido'}, {id: 2, name: '青森県', name_en: 'Aomori'}, {id: 3, name: '岩手県', name_en: 'Iwate'},
+    {id: 4, name: '宮城県', name_en: 'Miyagi'}, {id: 5, name: '秋田県', name_en: 'Akita'}, {id: 6, name: '山形県', name_en: 'Yamagata'},
+    {id: 7, name: '福島県', name_en: 'Fukusima'}, {id: 8, name: '茨城県', name_en: 'Ibaraki'}, {id: 9, name: '栃木県', name_en: 'Tochigi'},
+    {id: 10, name: '群馬県', name_en: 'Gunma'}, {id: 11, name: '埼玉県', name_en: 'Saitama'}, {id: 12, name: '千葉県', name_en: 'Chiba'},
+    {id: 13, name: '東京都', name_en: 'Tokyo'}, {id: 14, name: '神奈川県', name_en: 'Kanagawa'}, {id: 15, name: '新潟県', name_en: 'Niigata'},
+    {id: 16, name: '富山県', name_en: 'Toyama'}, {id: 17, name: '石川県', name_en: 'Ishikawa'}, {id: 18, name: '福井県', name_en: 'Fukui'},
+    {id: 19, name: '山梨県', name_en: 'Yamanashi'}, {id: 20, name: '長野県', name_en: 'Nagano'}, {id: 21, name: '岐阜県', name_en: 'Gifu'},
+    {id: 22, name: '静岡県', name_en: 'Shizuoka'}, {id: 23, name: '愛知県', name_en: 'Aichi'}, {id: 24, name: '三重県', name_en: 'Mie'},
+    {id: 25, name: '滋賀県', name_en: 'Shiga'}, {id: 26, name: '京都府', name_en: 'Kyoto'}, {id: 27, name: '大阪府', name_en: 'Osaka'},
+    {id: 28, name: '兵庫県', name_en: 'Hyogo'}, {id: 29, name: '奈良県', name_en: 'Nara'}, {id: 30, name: '和歌山県', name_en: 'Wakayama'},
+    {id: 31, name: '鳥取県', name_en: 'Tottori'}, {id: 32, name: '島根県', name_en: 'Shimane'}, {id: 33, name: '岡山県', name_en: 'Okayama'},
+    {id: 34, name: '広島県', name_en: 'Hiroshima'}, {id: 35, name: '山口県', name_en: 'Yamaguchi'}, {id: 36, name: '徳島県', name_en: 'Tokushima'},
+    {id: 37, name: '香川県', name_en: 'Kagawa'}, {id: 38, name: '愛媛県', name_en: 'Ehime'}, {id: 39, name: '高知県', name_en: 'Kochi'},
+    {id: 40, name: '福岡県', name_en: 'Fukuoka'}, {id: 41, name: '佐賀県', name_en: 'Saga'}, {id: 42, name: '長崎県', name_en: 'Nagasaki'},
+    {id: 43, name: '熊本県', name_en: 'Kumamoto'}, {id: 44, name: '大分県', name_en: 'Oita'}, {id: 45, name: '宮崎県', name_en: 'Miyazaki'},
+    {id: 46, name: '鹿児島県', name_en: 'Kagoshima'}, {id: 47, name: '沖縄県', name_en: 'Okinawa'}
+  ]
+end
