2e2dc6d5073ff133f033768ab8c83a2596593251
atamakonkurii <morisudesu@gmail.com>
2021-08-09 21:45:44 +0900

    記事詳細を修正

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nittai_black': '#222831',
+        'nittai_grey': '#393E46',
+        'nittai_teal': '#00ADB5',
+        'nittai_whitegrey': '#EEEEEE',
+      }
+    },
   },
   variants: {
     extend: {},
