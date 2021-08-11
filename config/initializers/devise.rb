b15f0565380a3f2e9577829f0ff0b962721e8e0c
atamakonkurii <morisudesu@gmail.com>
2021-08-11 21:28:36 +0900

    これを設定するとapp/view配下のerbが反映される

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,6 +309,8 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
+  config.scoped_views = true
+
   config.omniauth :facebook,Rails.application.credentials.facebook[:key],Rails.application.credentials.facebook[:secret], scope: 'email', info_fields: 'email', callback_url: "#{Rails.application.credentials.host[:url]}/users/auth/facebook/callback"
   config.omniauth :google_oauth2,Rails.application.credentials.google[:client_id],Rails.application.credentials.google[:client_secret], scope: 'email', redirect_uri: "#{Rails.application.credentials.host[:url]}/users/auth/google_oauth2/callback"
   OmniAuth.config.logger = Rails.logger if Rails.env.development?
