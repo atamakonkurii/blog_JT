a80c290908f258615a0fc3ab5854dc26fdbb091e
atamakonkurii <morisudesu@gmail.com>
2021-09-01 02:49:38 +0900

    一旦コミット

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :repo_url, "git@github.com:atamakonkurii/blog_JT.git"
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :branch, ENV['BRANCH'] || "main"
 
+set :puma_service_unit_name, 'puma_blog_JT_production.service'
+
 # Nginxの設定ファイル名と置き場所を修正
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
