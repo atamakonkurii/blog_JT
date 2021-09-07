4a26af243f5a878b80877ed38aa3d671800c309c
atamakonkurii <morisudesu@gmail.com>
2021-09-08 06:25:51 +0900

    carrieerwaveでtitle_imgaeをupできるように変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_130854) do
+ActiveRecord::Schema.define(version: 2021_09_07_204718) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_08_18_130854) do
     t.integer "main_language", null: false
     t.integer "user_id", null: false
     t.integer "place_id", null: false
+    t.string "title_image"
   end
 
   create_table "attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
