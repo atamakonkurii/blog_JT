3f804fec79787da2172b7f3ee8a70f7b4a4f1314
かずお <71773200+atamakonkurii@users.noreply.github.com>
2021-09-07 21:58:29 +0900

    Revert "画像がcloudfrontに向くように設定2"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,17 +5,4 @@ module ApplicationHelper
     end
     url_for(options)
   end
-
-  def cdn_ready_blob_path(attachment)
-    service = Rails.application.config.active_storage.service
-    case service
-    when :local
-      # 元々のヘルパ
-      rails_blob_path(attachment)
-    when :amazon
-      # S3上でのファイル名を取得してURLを組み立てる
-      key = attachment&.blob&.key
-      "https://static.nittai-one.com/#{key}"
-    end
-  end
 end
