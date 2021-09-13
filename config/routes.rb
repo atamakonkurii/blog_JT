0f48083c451aa50702bb41ee82c03b963c918c6d
atamakonkurii <morisudesu@gmail.com>
2021-09-14 07:38:31 +0900

    ユーザー情報を変更できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
+  get '/mypage' => 'users#mypage'
 
   root 'articles#index'
 
