dc8c77da46dce311f4ba44da88eaff449265a49b
atamakonkurii <morisudesu@gmail.com>
2021-09-14 07:06:33 +0900

    ユーザー情報閲覧ページを作成

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
       colors: {
         'nittai_black': '#222831',
         'nittai_grey': '#393E46',
+        'nittai_teal_thin' : '#8fecf3',
         'nittai_teal': '#00ADB5',
+        'nittai_teal_strong' : '#014c4e',
         'nittai_whitegrey': '#EEEEEE',
       }
     },
