3d0a96a907f1655740febf7d0bf0e7d177f5dbeb
atamakonkurii <morisudesu@gmail.com>
2021-09-27 20:50:05 +0900

    facebook ogpを表示できるように設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,34 @@ module ApplicationHelper
     end
     url_for(options)
   end
+
+  def default_meta_tags
+    {
+      site: '日台one!',
+      title: '日台友好',
+      reverse: true,
+      charset: 'utf-8',
+      description: '這是一個由日本台灣情侶經營的博客',
+      keywords: 'キーワード',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary',
+        site: '@atamakonkurii'
+      }
+    }
+  end
 end
