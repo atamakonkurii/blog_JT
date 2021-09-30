8382ef1006d6dd64d6b1ecba880a4e3bd4201caa
atamakonkurii <morisudesu@gmail.com>
2021-10-01 08:31:44 +0900

    UserのFactoryBotを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :user do
+    name { "Kazuki" }
+    sequence(:email) { |n| "tester#{n}@example.com" }
+    password { "AaBb1234" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "name、email、passwordが正しくある場合、有効な状態であること" do
-    user = User.new(
-      name: "Kazuki",
-      email: "test@example.com",
-      password: "AaAa5555"
-    )
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 
-    expect(user).to be_valid
+  it "重複したメールアドレスがある場合無効であること" do
+    FactoryBot.create(:user, email: "aaaa@example.com")
+    user = FactoryBot.build(:user, email: "aaaa@example.com")
+    user.valid?
+    expect(user.errors[:email]).to include("已經被使用")
   end
 
   it "passwordがnilの場合無効な状態であること" do
-    user = User.new(password: nil)
+    user = FactoryBot.build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include("不能為空白")
   end
