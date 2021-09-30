9936666d39cf8a721d03f7481154574cff3a1c69
atamakonkurii <morisudesu@gmail.com>
2021-09-30 22:20:52 +0900

    基本的なテストを追加

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "テスト１"
+  it "localeが日本の時日本語のコンテンツを返す" do
+    locale = 'ja'
+    article = Article.new(
+      content_ja: "日本語",
+      content_zh_tw: "台湾語"
+    )
 
-  it "テスト２"
+    expect(article.locale_judge_content(locale)).to eq "日本語"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'name、email、password_diegestが正しくある場合、有効な状態であること'
+  it "name、email、passwordが正しくある場合、有効な状態であること" do
+    user = User.new(
+      name: "Kazuki",
+      email: "test@example.com",
+      password: "AaAa5555"
+    )
+
+    expect(user).to be_valid
+  end
+
+  it "passwordがnilの場合無効な状態であること" do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("不能為空白")
+  end
 end
